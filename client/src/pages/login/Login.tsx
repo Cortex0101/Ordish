3b61981283c71c59d8d07a38719f01e0eedf4a15
Cortex0101 <ldeiruff@gmail.com>
2025-06-26 20:27:26 +0200

    Reinstall package.json

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -7,14 +7,10 @@ function Login() {
     const { t } = useTranslation('login');
 
     return (
-    <div className="app-container">
-      <div className="app-header text-center">
-        <Container>
-          <h1>🚀 Ordish - {t('welcome')}</h1>
-          <p className="lead mb-0">React + TypeScript + Express + Bootstrap + SCSS</p>
-        </Container>
-      </div>
-      </div>
+      <Container>
+        <h1>🚀 Ordish - {t('welcome')}</h1>
+        <p className="lead mb-0">React + TypeScript + Express + Bootstrap + SCSS</p>
+      </Container>
     );
 }
 
