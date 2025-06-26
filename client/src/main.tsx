05400cbd713b36046546cdb8d195e8c17114ab0d
Cortex0101 <ldeiruff@gmail.com>
2025-06-26 18:05:36 +0200

    Remove dark theme default

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.tsx'
 
-document.body.setAttribute('data-bs-theme', 'dark')
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
