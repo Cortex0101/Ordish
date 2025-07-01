2fcdf0aa624c7587f369834fb01754d241f127d1
Cortex0101 <ldeiruff@gmail.com>
2025-07-02 00:58:13 +0200

    Initial layout

--- client/src/pages/spelling-bee/SpellingBee.tsx
+++ client/src/pages/spelling-bee/SpellingBee.tsx
@@ -1,20 +1,72 @@
 import React from "react";
 import {
-  Container
+    Col,
+  Container,
+  Row
 } from "react-bootstrap";
 
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next"; // TODO: Import when implementing game content
 import "./SpellingBee.scss"; // Assuming you have a SpellingBee.scss for styles
 
+/*
+        <Col className="game-container" xs={12} md={6}>
+          W
+        </Col>
+
+        <Col className="progress-box" md={6}>
+          t
+        </Col>
+        <Col className="wordlist-box" md={6}>
+          x
+        </Col>
+*/
+
+
 const SpellingBee: React.FC = () => {
-  const { t } = useTranslation("profile");
+  // const { t } = useTranslation("spellingBee"); // TODO: Use when implementing game content
 
   return (
-    <Container className="settings-container">
-      <h2 className="h3 mb-4 page-title">{t("settings-title")}</h2>
-      {/* Settings content will go here */}
-      <p>{t("settings-description")}</p>
-      {/* Add more settings components as needed */}
+    <Container fluid className="spelling-bee-container">
+      {/* Mobile Layout: Stack vertically */}
+      <div className="d-md-none">
+        <Row className="mb-3">
+          <Col xs={12} className="progress-box mobile-progress">
+            Progress Box - 100% width, 20% height on mobile
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Col xs={12} className="wordlist-box mobile-wordlist">
+            Word List - 100% width, 20% height on mobile
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} className="game-container mobile-game">
+            Game Container - 100% width, 60% height on mobile
+          </Col>
+        </Row>
+      </div>
+
+      {/* Desktop Layout: Game left, Progress/Wordlist right */}
+      <Row className="d-none d-md-flex spelling-bee-desktop">
+        {/* Game Container - Left 50% */}
+        <Col md={6} className="game-container desktop-game">
+          Game Container - 50% width, 100% height on desktop
+        </Col>
+        
+        {/* Right side - Progress and Wordlist */}
+        <Col md={6} className="right-panel">
+          <Row className="h-100">
+            {/* Progress Box - Top 20% of right side */}
+            <Col xs={12} className="progress-box desktop-progress">
+              Progress Box - 50% width, 20% height on desktop
+            </Col>
+            {/* Word List - Bottom 80% of right side */}
+            <Col xs={12} className="wordlist-box desktop-wordlist">
+              Word List - 50% width, 80% height on desktop
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 };
